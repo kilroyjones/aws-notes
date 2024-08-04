c76fead230ba73b5489badc877c3fad684141515
kilroy <tsbach@gmail.com>
2024-08-04 10:55:01 +0800

    slight modifications

--- web/basic_s3_html_site/up.sh
+++ web/basic_s3_html_site/up.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
 source .env
-
 # Ensures that the site name is set
 if [ -z "$BASIC_SITE" ]; then
   echo "Error: BASIC_SITE environment variable is not set."
@@ -21,15 +18,20 @@ aws cloudformation create-stack \
 echo "Waiting for stack to be created..."
 aws cloudformation wait stack-create-complete --stack-name $BASIC_SITE
 
-
-# This uses the JSOn query language to get the BUCKET_NAME so we can 
+# This uses the JSON query language to get the BUCKET_NAME so we can 
 # output it to the terminal.
-export BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name $BASIC_SITE --query "Stacks[0].Outputs[?OutputKey=='BucketName'].OutputValue" --output text)
+export BUCKET_NAME=$(aws cloudformation describe-stacks \
+      --stack-name "$BASIC_SITE" \
+      --query "Stacks[0].Outputs[?OutputKey=='BucketName'].OutputValue" \
+      --output text)
 
 # Add our index.html to the newly created bucket.
 echo "Uploading index.html to bucket: $BUCKET_NAME"
 aws s3 cp ./index.html s3://$BUCKET_NAME/index.html --acl public-read
 
 # We again use the query language to pull out the website url.
-WEBSITE_URL=$(aws cloudformation describe-stacks --stack-name $BASIC_SITE --query "Stacks[0].Outputs[?OutputKey=='WebsiteURL'].OutputValue" --output text)
+export WEBSITE_URL=$(aws cloudformation describe-stacks \
+      --stack-name "$BASIC_SITE" \
+      --query "Stacks[0].Outputs[?OutputKey=='WebsiteURL'].OutputValue" \
+      --output text)
 echo "Website URL: $WEBSITE_URL"
